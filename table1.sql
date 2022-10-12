bd803fa6e0e2a3d1da88c9b56e7d9fe34e671d07
kunj <kunj.koladiya1972@gmail.com>
2022-10-12 02:26:45 -0700

    commit

--- table1.sql
+++ table1.sql
@@ -1,16 +1,25 @@
-create table client_master(
-   Clientno varchar(50) ,
-   Name varchar(50),
-   city varchar(50),
-   pincode numeric(38),
-   state varchar(50),
-   baldue numeric(38)
-);
-insert into client_master values('C00001', 'Ivan', 'Bombay', 400054, 'Maharashtra', 15000);
-insert into client_master values('C00002', 'Vandana', 'Madras', 780001, 'Tamilnadu', 0);
-insert into client_master values('C00003', 'Pramada', 'Bombay', 400057, 'Maharashtra', 5000);
-insert into client_master values('C00004', 'Basu', 'Bombay', 400056, 'Maharashtra', 0);
-insert into client_master values('C00005', 'Ravi', 'Delhi', 100001, '', 2000);
-insert into client_master values('C00006', 'Rukmini', 'Bombay', 400050, 'Maharashtra', 0);
-select * from client_master;
+-- create table client_master(
+--    Clientno varchar(50) ,
+--    Name varchar(50),
+--    city varchar(50),
+--    pincode numeric(38),
+--    state varchar(50),
+--    baldue numeric(38)
+-- );
+-- insert into client_master values('C00001', 'Ivan', 'Bombay', 400054, 'Maharashtra', 15000);
+-- insert into client_master values('C00002', 'Vandana', 'Madras', 780001, 'Tamilnadu', 0);
+-- insert into client_master values('C00003', 'Pramada', 'Bombay', 400057, 'Maharashtra', 5000);
+-- insert into client_master values('C00004', 'Basu', 'Bombay', 400056, 'Maharashtra', 0);
+-- insert into client_master values('C00005', 'Ravi', 'Delhi', 100001, '', 2000);
+-- insert into client_master values('C00006', 'Rukmini', 'Bombay', 400050, 'Maharashtra', 0);
+-- select * from client_master;
+-- select Name from client_master;
+-- select Name,city,state from client_master;
+-- select Name from client_master where city ="Bombay";
+-- update client_master set city='Bangloru' where Clientno ="C00005";
+-- select * from client_master;
+-- update client_master set state='Gujrat' where Clientno ="C00005";
+-- update client_master set baldue='1000' where Clientno ="C00001";
+
+drop table client_master;
 
--- table2.sql
+++ table2.sql
@@ -1,24 +1,33 @@
-create table product_master(
-    product_no varchar(50),
-    description varchar(50),
-    profit_percentage numeric(38,2),
-    unit_measured varchar(50),
-    qty_on_hand numeric(38),
-    recorder_lvl numeric(38),
-    sell_price numeric(38),
-    cost_price numeric(38)
-);
-insert into product_master values('P00001','1.44 floppies',5,'piece',100,20,525,500);
-insert into product_master values('P03453','Monitors',6,'piece',10,3,12000,11200);
-insert into product_master values('P06734','mouse',5,'piece',20,5,1050,500);
-insert into product_master values('P07865','1.22 floppies',5,'piece',100,20,525,500);
-insert into product_master values('P07868','keybords',2,'piece',10,3,3150,1050);
-insert into product_master values('P07885','CD drive',2.50,'piece',10,3,5250,5100);
-insert into product_master values('P07965','540 HDD',4,'piece',10,3,8400,8000);
-insert into product_master values('P07975','1.44 drive',5,'piece',10,3,1050,1000);
-insert into product_master values('P08865','1.22 drive',5,'piece',2,3,1050,1000);
+-- create table product_master(
+--     product_no varchar(50),
+--     description varchar(50),
+--     profit_percentage numeric(38,2),
+--     unit_measured varchar(50),
+--     qty_on_hand numeric(38),
+--     recorder_lvl numeric(38),
+--     sell_price numeric(38),
+--     cost_price numeric(38)
+-- );
+-- insert into product_master values('P00001','1.44 floppies',5,'piece',100,20,525,500);
+-- insert into product_master values('P03453','Monitors',6,'piece',10,3,12000,11200);
+-- insert into product_master values('P06734','mouse',5,'piece',20,5,1050,500);
+-- insert into product_master values('P07865','1.22 floppies',5,'piece',100,20,525,500);
+-- insert into product_master values('P07868','keybords',2,'piece',10,3,3150,1050);
+-- insert into product_master values('P07885','CD drive',2.50,'piece',10,3,5250,5100);
+-- insert into product_master values('P07965','540 HDD',4,'piece',10,3,8400,8000);
+-- insert into product_master values('P07975','1.44 drive',5,'piece',10,3,1050,1000);
+-- insert into product_master values('P08865','1.22 drive',5,'piece',2,3,1050,1000);
 
-select * from product_master;
+-- select * from product_master;
+-- select description,sell_price from product_master;
+-- select *,description,sell_price from product_master;
+-- select description "Description" from product_master;
+
+-- select product_no from product_master;
+drop table product_master;
+
+
+
 
 
 
